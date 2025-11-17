6b39be4469a80626c541b5109a3dc653b28e720c
pravee-n03 <praveen10class@gmail.com>
2025-11-17 21:19:49 +0530

    cache

--- shared/utils/auth/tokenCookei.js
+++ shared/utils/auth/tokenCookei.js
@@ -3,43 +3,43 @@ import Cookies from "cookies";
 // set access and refresh JWT tokens as httpOnly cookies
 export const cookieGenerator = (tknInf, token, req, res) => {
   const cookies = new Cookies(req, res);
-  const isProduction = process.env.NODE_ENV === 'production';
+  const isSecure = req.connection.encrypted; // Only set secure if connection is HTTPS
 
   cookies.set(tknInf.type, token, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'lax' : 'lax',
+    secure: isSecure,
+    sameSite: 'lax',
     maxAge: tknInf.age * 1000,
   });
 };
 
 export const deleteCookie = (type, req, res) => {
   const cookies = new Cookies(req, res);
-  const isProduction = process.env.NODE_ENV === 'production';
-  cookies.set(type, "", { 
-    httpOnly: true, 
-    secure: isProduction,
-    sameSite: isProduction ? 'lax' : 'lax',
-    maxAge: 1 
+  const isSecure = req.connection.encrypted; // Only set secure if connection is HTTPS
+  cookies.set(type, "", {
+    httpOnly: true,
+    secure: isSecure,
+    sameSite: 'lax',
+    maxAge: 1
   });
 };
 
 export const isAdminCookie = (req, res, isAdmin) => {
   const cookies = new Cookies(req, res);
-  const isProduction = process.env.NODE_ENV === 'production';
-  cookies.set("isAdmin", `${isAdmin}`, { 
+  const isSecure = req.connection.encrypted; // Only set secure if connection is HTTPS
+  cookies.set("isAdmin", `${isAdmin}`, {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'lax' : 'lax',
+    secure: isSecure,
+    sameSite: 'lax',
   });
 };
 export const isAdminDelete = (req, res) => {
   const cookies = new Cookies(req, res);
-  const isProduction = process.env.NODE_ENV === 'production';
-  cookies.set("isAdmin", "", { 
+  const isSecure = req.connection.encrypted; // Only set secure if connection is HTTPS
+  cookies.set("isAdmin", "", {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'lax' : 'lax',
-    maxAge: 1 
+    secure: isSecure,
+    sameSite: 'lax',
+    maxAge: 1
   });
 };
